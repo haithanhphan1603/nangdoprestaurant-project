804f727123e5567f46f5c81b751d5466edba8cb7
hai-phan-163 <haithanhphan163@gmail.com>
2021-10-27 10:23:24 +0700

    update anchor tag

--- js/script.js
+++ js/script.js
@@ -34,18 +34,19 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
     const href = link.getAttribute("href");
 
     // Scroll back to top
-    if (href === "#")
+    if (href === "#") {
+      e.preventDefault();
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
-
+    }
     // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
+      e.preventDefault();
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
